308c14536613c076a6652110ddc8aa6a864a3d62
titanium <titaniumdev123@gmail.com>
2022-05-10 13:21:14 +0000

    init commit

---
+++ lib/views/booking_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookingView extends StatelessWidget {
+  const BookingView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Bookings')),
+    );
+  }
+}
--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utils.dart';
+import 'package:movie_app/views/booking_view.dart';
+import 'package:movie_app/views/home_view.dart';
+import 'package:movie_app/views/profile_view.dart';
 
 class DashBoardView extends StatefulWidget {
   const DashBoardView({ Key? key }) : super(key: key);
@@ -8,10 +12,38 @@ class DashBoardView extends StatefulWidget {
 }
 
 class _DashBoardViewState extends State<DashBoardView> {
+  int currentIndex = 0;
+  final pages = const [
+    HomeView(),
+    BookingView(),
+    ProfileView()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: customWhite,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => setState(() {
+          currentIndex = value;
+        }),
+        currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Bookings'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Profile'
+          ),
+        ]
+      ),
     );
   }
 }
