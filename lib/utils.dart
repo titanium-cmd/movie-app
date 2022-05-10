f6e41cde2b1278c8f99df913512ad2005f86b04c
titanium <titaniumdev123@gmail.com>
2022-05-10 13:07:04 +0000

    init commit

---
+++ lib/utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const customWhite = Color(0xFFFCFCFD);
+const customLightGrey = Color(0xFF9A9A9A);
+const customDarkGrey = Color(0xFF5D5E5F);
+const customBrown = Color(0xFF583437);
+const customBlack = Color(0xFF0A090A);
---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DashBoardView extends StatefulWidget {
+  const DashBoardView({ Key? key }) : super(key: key);
+
+  @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
